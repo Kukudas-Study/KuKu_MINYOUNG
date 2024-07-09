34bc40eaa18d4d2cbf57eecc39563c1b0a0b96f9
ammy1120 <mogul_daybed_0n@icloud.com>
2024-07-09 12:34:17 +0900

    프로그래머스 42862번, 42885번 풀이 주석 추가

--- week_2/P_42862.java
+++ week_2/P_42862.java
@@ -11,26 +11,39 @@ public final class P_42862 {
     }
 
     public int solution(int n, int[] lost, int[] reserve) {
+        // 체육복을 가져온 학생들을 저장
         var reserves = new LinkedList<Integer>();
         for (int student : reserve) {
             reserves.add(student);
         }
 
+        // 체육복을 잃어버린 학생들을 저장
         var losts = new LinkedList<Integer>();
         for (int student : lost) {
+            // "여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다"
+            // 이 조건에 만족하는 학생은 reserves, losts 둘 다 제외시킨다.
             if (!reserves.remove((Object) student)) {
                 losts.add(student);
             }
         }
 
+        // 번호 순으로 정렬
         reserves.sort(Comparator.reverseOrder());
 
+        // 체육복을 가져온 학생들을 순회
         for (int student : reserves) {
+            // 앞 번호가 잃어버린 학생에 해당하는지 확인하기 위해 remove를 호출한다.
+            //
+            // 호출 결과가 참인 경우 앞 번호는 잃어버린 학생에 해당하므로,
+            // losts의 크기는 1 줄어들게 된다.
+            //
+            // 호출 결과가 거짓인 경우, 뒷 번호에도 remove를 호출한다.
             if (!losts.remove((Object) (student + 1))) {
                 losts.remove((Object) (student - 1));
             }
         }
 
+        // 전체 학생 수 - 잃어버린 수 = 체육수업을 들을 수 있는 학생의 최댓값
         return n - losts.size();
     }
 }
--- week_2/P_42885.java
+++ week_2/P_42885.java
@@ -9,30 +9,44 @@ public final class P_42885 {
     }
 
     public int solution(int[] people, int limit) {
+        // 무게 순으로 정렬한다.
         Arrays.sort(people);
 
+        // answer 답
+        // j 배열의 마지막 index
         var answer = 0;
         var j = people.length - 1;
 
+        // 사람들을 순회한다.
         loop:
         for (int i = 0; i < people.length; i++) {
+            // -1은 이미 탑승한 사람을 의미한다.
+            // 왼쪽의 사람이 탑승한 사람인 경우, 스킵한다.
             var x = people[i];
             if (x == -1) {
                 continue;
             }
 
+            // 배열의 맨 뒷사람부터 현재 사람의 바로 뒷사람까지 순회한다.
             for (; j > i; j--) {
                 var y = people[j];
+                // 순회 중인 사람이 탑승한 사람이거나,
+                // 현재 사람과 순회 중인 사람 무게의 합이 한도를 초과하면 스킵한다.
                 if (y == -1 || x + y > limit) {
                     continue;
                 }
 
+                // 한도 이내라면, 둘 다 탑승한 사람으로 처리하고 카운팅한다.
                 people[i] = -1;
                 people[j] = -1;
                 answer++;
+
+                // 이 때, 바깥 순회에 대하여 스킵한다.
                 continue loop;
             }
 
+            // 같이 타고갈 수 있는 사람이 없으므로,
+            // 탑승한 사람으로 처리하고 카운팅한다.
             people[i] = -1;
             answer++;
         }
