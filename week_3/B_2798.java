00655a80e3ffa7a2eb7e4b1ab8c8615dda5f15c5
ammy1120 <mogul_daybed_0n@icloud.com>
2024-07-12 16:04:15 +0900

    백준 2798번 풀이, 다시 풀 예정

---
+++ week_3/B_2798.java
@@ -0,0 +1,68 @@
+package week_3;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public final class B_2798 {
+    public static void main(String[] args) throws IOException {
+        var reader = new BufferedReader(new InputStreamReader(System.in));
+        var writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        var input = reader.readLine().split(" ", 2);
+
+        var N = Integer.parseInt(input[0]);
+        var M = Integer.parseInt(input[1]);
+
+        var numbers = new int[N];
+        var strings = reader.readLine().split(" ", N);
+        for (int index = 0; index < strings.length; index++) {
+            numbers[index] = Integer.parseInt(strings[index]);
+        }
+
+        writer.write(String.valueOf(solution(M, numbers)));
+        writer.flush();
+        writer.close();
+        reader.close();
+    }
+
+    // 다시 풀 예정
+    public static int solution(int M, int[] numbers) {
+        var dest = new ArrayList<Integer>();
+
+        var visited = new boolean[100_000];
+        for (int number : numbers) {
+            visited[number] = true;
+            comb(number, 1, numbers, visited, M, dest);
+            visited[number] = false;
+        }
+
+        return dest.stream().mapToInt(it -> it).max().orElseThrow();
+    }
+
+    public static void comb(
+            int sum,
+            int depth,
+            int[] numbers,
+            boolean[] visited,
+            int M,
+            ArrayList<Integer> dest
+    ) {
+        if (depth == 3) {
+            if (sum <= M) {
+                dest.add(sum);
+            }
+
+            return;
+        }
+
+        for (int number : numbers) {
+            if (visited[number]) {
+                continue;
+            }
+
+            visited[number] = true;
+            comb(sum + number, depth + 1, numbers, visited, M, dest);
+            visited[number] = false;
+        }
+    }
+}
