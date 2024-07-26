99084f2220ec1073575a1c55b0a80ce5aa360c99
ammy1120 <mogul_daybed_0n@icloud.com>
2024-07-26 13:38:31 +0900

    백트래킹 문제들 패키지 이동

--- week_5/B_15652.java
+++ week_5/B_15652.java
@@ -1,8 +1,8 @@
-package week_4;
+package week_5;
 
 import java.io.*;
 
-public final class B_15651 {
+public final class B_15652 {
     public static void main(String[] args) throws IOException {
         var reader = new BufferedReader(new InputStreamReader(System.in));
         var writer = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -12,16 +12,21 @@ public final class B_15651 {
         var N = Integer.parseInt(input[0]);
         var M = Integer.parseInt(input[1]);
         var builder = new StringBuilder();
-        track(N, M, 0, new int[M], builder);
+        track(1, N, M, 0, new int[M], builder);
         writer.write(builder.toString());
         writer.flush();
         writer.close();
         reader.close();
     }
 
-    // visited 없는 백트래킹 구현
+    // visited 없는 백트래킹 구현 +
+    //
+    // 뽑을 수들은 뽑은 수의 최대를 초과해야 한다.
+    // 근데 자기도 뽑을 수 있으니까 초과 X 이상 O
+    //
     // 백트래킹 구현 설명은 생략
     public static void track(
+            int start,
             int N,
             int M,
             int depth,
@@ -44,9 +49,9 @@ public final class B_15651 {
             return;
         }
 
-        for (int number = 1; number <= N; number++) {
+        for (int number = start; number <= N; number++) {
             array[depth] = number;
-            track(N, M, depth + 1, array, builder);
+            track(number, N, M, depth + 1, array, builder);
         }
     }
 }
